dcdb652d5708eec13147128b6d060b751622ae23
Todmy <tolok.d@gmail.com>
2016-07-09 20:33:26 +0300

    Finished $timeout testing

--- spec/movie-app/search.controller.spec.js
+++ spec/movie-app/search.controller.spec.js
@@ -1,13 +1,20 @@
 describe('Search Controller', function() {
   var $controller;
   var $location;
+  var $timeout;
 
   beforeEach(module('movieApp'));
 
-  beforeEach(inject(function(_$controller_, _$location_) {
+  beforeEach(inject(function(_$controller_, _$location_, _$timeout_) {
     $location = _$location_;
-    $controller = _$controller_.bind(null, 'SearchController', { $location: $location });
+    $timeout = _$timeout_;
+    $controller = _$controller_.bind(null, 'SearchController', { $location: $location, $timeout: $timeout });
   }));
+
+  afterEach(function() {
+    $timeout.verifyNoPendingTasks();
+  });
+
   it('should redirect to the query results page for non-empty query', function() {
     var vm = $controller({ query: 'star wars'});
     vm.search();
@@ -18,5 +25,28 @@ describe('Search Controller', function() {
     var vm = $controller({ query: ''});
     vm.search();
     expect($location.url()).toBe('');
+  });
+
+  it('should redirect after 1 second of keyboard inactivity', function() {
+    var vm = $controller({ query: 'star wars' });
+    vm.keyup();
+    $timeout.flush();
+    expect($location.url()).toBe('/results?q=star%20wars');
+  });
+
+  it('should cancel timeout in keydown', function() {
+    var vm = $controller({ query: 'star wars' });
+    vm.keyup();
+    vm.keydown();
+
+    expect($timeout.verifyNoPendingTasks).not.toThrow();
+  });
+
+  it('should cancel timeout in search', function() {
+    var vm = $controller({ query: 'star wars' });
+    vm.keyup();
+    vm.search();
+
+    expect($timeout.verifyNoPendingTasks).not.toThrow();
   });
 });
--- src/movie-app/search.controller.js
+++ src/movie-app/search.controller.js
@@ -1,16 +1,27 @@
 angular.module('movieApp')
   .controller('SearchController', SearchController);
 
-SearchController.$inject = [ '$location' ];
+SearchController.$inject = [ '$location', '$timeout' ];
 
-function SearchController($location) {
+function SearchController($location, $timeout) {
   var vm = this;
 
   vm.search = searchMovie;
+  vm.keyup = keyup;
+  vm.keydown = keydown;
 
   function searchMovie() {
+    keydown();
     if(vm.query) {
       $location.path('/results').search('q', vm.query);
     }
+  }
+
+  function keyup() {
+    vm.timeoutFn = $timeout(searchMovie, 1000);
+  }
+
+  function keydown() {
+    $timeout.cancel(vm.timeoutFn);
   }
 }
